1372b412515cce69b0f6c09f0041c40af8b1f4a1
Weikang Gong <gwk_051905@163.com>
2017-07-12 14:22:40 +0800

    Update BWAS_glm.m

--- BWAS_glm.m
+++ BWAS_glm.m
@@ -1,10 +1,11 @@
 function BWAS_glm(images,design,mask)
 %This function performs whole-brain BWAS
-%        BWAS_glm(images,design)
+%        BWAS_glm(images,design,mask)
 %Input:  images: cell (time*voxel per cell)
 %        design: 2D matrix (num_sample, p variable), the first one is the
 %        target variable
-%Output: statistical parametric Maps saved in .mat format
+%        mask: the voxel you want to analyse is 1 , otherwise 0.
+%Output: Statistical parametric Maps saved in .mat format
 %%
 disp('Checking image quality...');
 n_voxel=size(images{1},2);
@@ -19,7 +20,7 @@ for i=1:n_sample
     images{i}(:,ind)=0;
 end
 if n_bad>0
-    disp(['Your images contrains ',num2str(n_bad),...
+    disp(['Your images contain ',num2str(n_bad),...
         ' "bad" voxels, please check "bad_voxels.mat"!'])
     save('bad_voxels.mat','bad_voxels');
 else
@@ -31,7 +32,7 @@ if class(design)~='double'
     error('The data type of design matrix should be double!')
 end
 if sum(isnan(design(:)))>0
-    error('Your phenotype matrix contrains NaN, please check it!');
+    error('Your phenotype matrix contains NaN, please check it!');
 end
 disp('finished! ')
 
@@ -57,7 +58,7 @@ for i=1:ind_end
     rr2=reshape(rr1,[n_sample,nn]);
     
     %general linear model on FCs...
-    disp(['Calculating the Statistical Parametirc Maps... '...
+    disp(['Calculating the Statistical Parametric Maps... '...
         num2str(i),'/',num2str(ind_end),'. '])
     ts=BWAS_Tregression(design,rr2);
     stat_map=reshape(ts,[n_voxel,(en1-st1+1)]);
